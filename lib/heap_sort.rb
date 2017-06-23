8fdec2ff415c16a565bb1a8fd84ee7ff65d9e20e
Bruk Argaw <bkargaw@gmail.com>
2017-06-23 15:28:52 -0700

    finished implementing a heapsort

--- lib/heap_sort.rb
+++ lib/heap_sort.rb
@@ -2,5 +2,13 @@ require_relative "heap"
 
 class Array
   def heap_sort!
+    heap = BinaryMinHeap.new
+    each { |el| heap.push(el) }
+    i = 0
+    while heap.count > 0
+      self[i] = heap.extract
+      i += 1
+    end
+
   end
 end
